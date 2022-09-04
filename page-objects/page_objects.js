0cc4f52699e653ddfe371f808698694ab3b37829
Ronaldo <ronaldo.a.f@gmail.com>
2022-09-04 12:03:07 -0300

    intital

--- page-objects/page_objects.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    elements: {
-        word_list: {
-            selector: 'input[type=number]'
-        }
-    }
-};
--- update_bulma.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-wget https://raw.githubusercontent.com/jgthms/bulma/master/css/bulma.min.css -O calculator/bulma.min.css
